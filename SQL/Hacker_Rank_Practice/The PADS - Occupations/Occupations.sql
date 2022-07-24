fcbf48a01bdfdbc79a38f5f502dac3d58b115f3a
Darren Chua <44729635+darrenCWJ@users.noreply.github.com>
2022-07-25 01:28:50 +0800

    Addition of new SQL

---
+++ SQL/Hacker_Rank_Practice/The PADS - Occupations/Occupations.sql
@@ -0,0 +1,13 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT CONCAT(NAME, '(' , SUBSTRING(OCCUPATION,1,1) , ')')
+FROM OCCUPATIONS
+ORDER BY NAME;
+SELECT CONCAT('There are a total of ', COUNT(OCCUPATION)," ", lower(OCCUPATION), "s.")
+FROM OCCUPATIONS
+GROUP BY OCCUPATION
+ORDER BY COUNT(OCCUPATION), OCCUPATION;
+
+
